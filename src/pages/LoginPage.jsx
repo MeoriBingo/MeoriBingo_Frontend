bbc83934ca4c1079b4f869c69bf1d0a45d54eaad
roksf0130 <roksf0130@gmail.com>
2026-04-18 01:35:59 +0900

    yaml 수정 및 빌드

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,9 +2,6 @@ import { useEffect } from 'react';
 import './LoginPage.css'
 
 function LoginPage() {
-    console.log("ALL ENV:", import.meta.env);
-    console.log("KAKAO KEY:", import.meta.env.VITE_KAKAO_JS_KEY);
-
     useEffect(() => {
         if (window.Kakao && !window.Kakao.isInitialized()) {
             window.Kakao.init(import.meta.env.VITE_KAKAO_JS_KEY);
